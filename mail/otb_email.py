266834a7189d3de6a5cb8e00251322d876cfc047
LHAnh <164833189+LHAnh2809@users.noreply.github.com>
2024-05-29 10:15:19 +0700

    Update otb_email.py

--- mail/otb_email.py
+++ mail/otb_email.py
@@ -1,19 +1,18 @@
 import smtplib
 from email.mime.text import MIMEText
 
-def send_otp_email(receiver_email, otp_code, name):
+def send_email_temp(receiver_email, contents, title):
 
-    contents = f"Xin chào {name}!\n\nMã của bạn là {otp_code}\n\nNhóm,\nOrder food"
-    mail = smtplib.SMTP('smtp.gmail.com', 587)
+    mail = smtplib.SMTP_SSL("smtp.gmail.com", 465)
     mail.ehlo()
     mail.starttls()
     email_sent = 'busterdust5@gmail.com'
     msg = MIMEText(contents)
-    msg['Subject'] = f"Mã của bạn - {otp_code}"
+    msg['Subject'] = title
     msg['From'] = email_sent
     msg['To'] = receiver_email
 
-    mail.login('busterdust5@gmail.com','jsqn jybb llhb cfxw')
+    mail.login('busterdust5@gmail.com','mmfg lxud resj iqin')
 
     header = 'To:'+email_sent+'\n'+'From: '+receiver_email+'\n'+'subject:testmail\n'
     content = header + contents
